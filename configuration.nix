3634a251578162549b20dee33d28b7703052fa74
Boar Laurentiu <boarlaurentiu7@gmail.com>
2026-06-29 10:34:40 +0300

    update

--- configuration.nix
+++ configuration.nix
@@ -247,6 +247,13 @@ hardware.graphics = {
     enable32Bit = true; # Fixes the 32-bit Steam / Wine driver crashes
   };
 
+environment.sessionVariables = {
+  # Forces applications to use the NVIDIA GPU for OpenGL
+  __NV_PRIME_RENDER_OFFLOAD = "1";
+  __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+  __VK_LAYER_NV_optimus = "NVIDIA_only";
+};
+
 fonts.packages = with pkgs; [
   jetbrains-mono
   nerd-fonts.jetbrains-mono
