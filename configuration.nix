d281be0d74366cf894f8e7d59b7d8e12da82c0f2
Boar Laurentiu <boarlaurentiu7@gmail.com>
2026-08-13 07:40:14 +0300

    update

--- configuration.nix
+++ configuration.nix
@@ -100,6 +100,12 @@
   };
 };
 
+   services.picom = {
+   enable = true;
+   backend = "glx";
+   vSync = true;
+};
+
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.laur = {
     isNormalUser = true;
